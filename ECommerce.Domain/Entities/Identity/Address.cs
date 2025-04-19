6df2c5d2e99f0417164ba5d229ba0ea7653a713b
UsfHajaj <yosefys236@gmail.com>
2025-04-19 16:42:39 +0200

    New Update

---
+++ ECommerce.Domain/Entities/Identity/Address.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Domain.Entities.Identity
+{
+    public class Address
+    {
+        public int Id { get; set; }
+        public string? UserId { get; set; }
+        public string? Street { get; set; }
+        public string? City { get; set; }
+        public string? State { get; set; }
+        public string? Country { get; set; }
+        public string? Zipcode { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+    }
+}
